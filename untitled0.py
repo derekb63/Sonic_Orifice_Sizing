5b52b9f71beeec223d7474db43e5b5e40d289437
Derek Bean <beande@regonstate.edu>
2017-03-30 16:38:54 -0700

    File to caclulate the mass flow rate through a sonic orifice given a list of variables and solve for the unknown ones

---
+++ untitled0.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Sympy solver for calculating the unknown variables in the mass flow rate
+through a sonic orifice
+
+@author: beande
+"""
+
+from sympy.solvers import solve
+from sympy import Symbol
+import cantera as ct
+import numpy as np
+
+m_dot = Symbol('m')
+A = Symbol('A')
+P_o = Symbol('P')
+k = Symbol('k')
+C_d = Symbol('Cd')
+[rho, k, MW] = Calc_Props(Gas, T, P_u)
+R = 8314
+T = 298
+
+def m_dot(Orifice, or_unit, P_u, p_unit, T, Gas, C_d=.99):
+    Orifice = conv_in_m(Orifice, or_unit, 'm')
+    P_u = conv_Pa_psi(P_u, p_unit, 'Pa')
+    A = A_orf(Orifice)
+    [rho, k, MW] = Calc_Props(Gas, T, P_u)
+    R = ct.gas_constant
+    m_dot = A*P_u*k*C_d*np.sqrt((2/(k+1))**((k+1)/(k-1)))/np.sqrt((R*T)/(k*MW))
+    return m_dot
